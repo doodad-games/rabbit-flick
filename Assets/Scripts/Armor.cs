d51069afdf0f445bd1795bcd39d6bb5b3daa2a00
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-02-04 12:46:46 +1100

    Armor disappearing

---
+++ Assets/Scripts/Armor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Armor : MonoBehaviour
+    {
+        float _knockOffForce = 14.1f;
+        float _disapearTime = 2.1f;
+
+        public void InitializeArmor(float armourKnockOffForce, float armourDisapearTime)
+        {
+            _knockOffForce = armourKnockOffForce;
+            _disapearTime = armourDisapearTime;
+
+            StartCoroutine(ArmourLifeCycle());
+        }
+
+        IEnumerator ArmourLifeCycle()
+        {
+            transform.parent = null;
+            gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+            Rigidbody armourRigidbody = gameObject.AddComponent<Rigidbody>();
+            armourRigidbody.AddExplosionForce(
+                _knockOffForce,
+                 new Vector3(
+                     Random.Range(1.0f,-1.0f),
+                     1,
+                     Random.Range(1.0f,-1.0f)),
+                _knockOffForce, _knockOffForce,ForceMode.Impulse);
+            yield return new WaitForSeconds(_disapearTime);
+            Debug.Log($"transform.localScale.magnitude = {transform.localScale.magnitude}");
+            while (transform.localScale.magnitude > 0.1f)
+            {
+                Vector3 newScale  = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * (1-Time.deltaTime*5);
+                transform.localScale = newScale;
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/BunnyArmored.cs
+++ Assets/Scripts/BunnyArmored.cs
@@ -4,30 +4,15 @@ namespace DefaultNamespace
     public class BunnyArmored : MonoBehaviour
     {
         [SerializeField] GameObject armour;
-        [SerializeField] float armourFallForce = 14.1f;
-        [SerializeField] float armourDisapearTime = 2.1f;
+        [SerializeField] float _armourKnockOffForce = 14.1f;
+        [SerializeField] float _armourDisapearTime = 2.1f;
+
         public void DropArmour()
-        {
-            if (armour!= null)
-            {
-                armour.transform.parent = null;
-                armour.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-                Rigidbody armourRigidbody = armour.AddComponent<Rigidbody>();
-                armourRigidbody.AddExplosionForce(
-                    armourFallForce,
-                    new Vector3(
-                        Random.Range(1.0f,-1.0f),
-                        1,
-                        Random.Range(1.0f,-1.0f)),
-                    armourFallForce, armourFallForce,ForceMode.Impulse);
-                Invoke(nameof(DestroyArmour), armourDisapearTime);
-            }
-        }
-        void DestroyArmour()
         {
             if (armour != null)
             {
-                Destroy(armour);
+                armour.AddComponent<Armor>()
+                    .InitializeArmor(_armourKnockOffForce, _armourDisapearTime);
             }
         }
     }
