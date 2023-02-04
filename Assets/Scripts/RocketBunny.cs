ee7d03d18e34b6aba4fc92f499fcc52e4ee9cef7
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-02-04 21:06:53 +1100

    Rocket Bunny

---
+++ Assets/Scripts/RocketBunny.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class RocketBunny : MonoBehaviour
+{
+    [SerializeField] float rocketLaunchTime = 1.2f;
+    [SerializeField] float launchSpeed = 24f;
+    Bunny _bunny;
+
+    public void OnEnable()
+    {
+        _bunny = GetComponent<Bunny>();
+        _bunny.OnDestroyed += LaunchRocket;
+    }
+    public void OnDisable() =>
+        _bunny.OnDestroyed -= LaunchRocket;
+
+    void LaunchRocket()
+    {
+        StartCoroutine(LifeCycle());
+    }
+    
+    IEnumerator LifeCycle()
+    {
+        float timer = rocketLaunchTime;
+        GameObject parentObject = new GameObject();
+        transform.parent = parentObject.transform;
+        while (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            transform.Translate(-transform.right * launchSpeed * Time.deltaTime);
+            if (timer > rocketLaunchTime * 0.7f)
+            {
+                parentObject.transform.Rotate(parentObject.transform.forward * 2.8f);
+            }
+            else
+            {
+                parentObject.transform.rotation = Quaternion.Euler(new Vector3(0,0,90));
+                transform.Translate(-transform.right * launchSpeed * Time.deltaTime);
+            }
+            yield return null;
+        }
+        Destroy(transform.parent.gameObject);
+    }
+}
