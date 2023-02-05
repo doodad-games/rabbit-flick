494b56ff53759b3287767f714a67a93d22df128e
Kieran <78832088+KanserReverie@users.noreply.github.com>
2023-02-05 17:57:51 +1100

    Ninja

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,8 +3,7 @@
 public abstract class Movement : MonoBehaviour
 {
     protected bool HasTarget { get; private set; }
-    public Vector3 Target =>
-        _targetTransform == null ? _targetPoint : _targetTransform.position;
+    public Vector3 Target => _targetTransform == null ? _targetPoint : _targetTransform.position;
 
     Transform _targetTransform;
     Vector3 _targetPoint;
